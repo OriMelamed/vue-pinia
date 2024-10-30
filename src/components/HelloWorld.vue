b352b8d6978ae7912c70df22fb029029923dc598
Ori Melamed <ori.melamed@gmail.com>
2024-10-30 13:45:19 +0200

    create body

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,9 @@ const count = ref(0)
 <template>
   <h1>Ori</h1>
   <h2>Test title</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequatur? Facilis ratione illum et
+    reprehenderit, cum fugiat atque necessitatibus aut, nulla excepturi eligendi odio eius porro id odit aliquam soluta!
+  </p>
 </template>
 
 <style scoped>
