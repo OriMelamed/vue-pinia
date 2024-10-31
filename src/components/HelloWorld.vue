1331d6546c02f50594f1de87423bae0194c2ad20
Ori Melamed <ori.melamed@gmail.com>
2024-10-31 09:23:16 +0200

    Finish table page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,8 +4,6 @@ import FooterBanner from './FooterBanner.vue';
 </script>
 
 <template>
-  <h1>Ori</h1>
-  <h2>Test title</h2>
   <name-table />
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequatur? Facilis ratione illum et
     reprehenderit, cum fugiat atque necessitatibus aut, nulla excepturi eligendi odio eius porro id odit aliquam soluta!
