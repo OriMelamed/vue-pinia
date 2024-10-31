2487636a4ccd0bcf84ebab2dcbe7ed24045af144
Ori Melamed <ori.melamed@gmail.com>
2024-10-31 10:57:11 +0200

    Print search value from child component in parent component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import NameTable from './NameTable.vue';
 import FooterBanner from './FooterBanner.vue';
+import SearchCard from './SearchCard.vue';
+
+const searchHandler = (searchValue: string) => {
+  console.log("Searching from HelloWorld:", searchValue);
+};
+
 </script>
 
 <template>
+  <search-card @search-click="searchHandler" />
   <name-table />
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, consequatur? Facilis ratione illum et
     reprehenderit, cum fugiat atque necessitatibus aut, nulla excepturi eligendi odio eius porro id odit aliquam soluta!
